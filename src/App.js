454aeef94d34178ece1b7bc99dcf509726a584f1
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-03 20:57:50 +0545

    getting data from outside source on component mounting

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "frankenstein",
-          id: 1
-        },
-        {
-          name: "dracula",
-          id: 2
-        },
-        {
-          name: "zombie",
-          id: 3
-        }
-      ]
+      monsters: []
     };
+  }
+
+  // called after constructor and before mounting this component
+  // onto the browser
+  componentDidMount() {
+    // request to the url
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users =>
+        this.setState({
+          monsters: users
+        })
+      );
   }
 
   render() {
