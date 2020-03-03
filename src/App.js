51644de941e6811931bde5d607fc0b9b809348ff
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-03 20:34:09 +0545

    formatted

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "hakuna matata" })}>Change Text</button>
+          <button onClick={() => this.setState({ string: "hakuna matata" })}>
+            Change Text
+          </button>
         </header>
       </div>
     );
