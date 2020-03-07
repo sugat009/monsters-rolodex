58b9fa5427116ce5d79cc553530bde4ea00f0e29
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-07 12:16:26 +0545

    Updates

---
+++ precache-manifest.536ab904bad9809fdd59b729b9781822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a871521feb7005757351d37ef3e942",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "187c05da9193948986a5",
+    "url": "/monsters-rolodex/static/css/main.bc997bb8.chunk.css"
+  },
+  {
+    "revision": "2dcba8f4d9675eb0f4a8",
+    "url": "/monsters-rolodex/static/js/2.c8b6800c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.c8b6800c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187c05da9193948986a5",
+    "url": "/monsters-rolodex/static/js/main.3ec3f065.chunk.js"
+  },
+  {
+    "revision": "5018ed9a0afe64468450",
+    "url": "/monsters-rolodex/static/js/runtime-main.61c619d4.js"
+  }
+]);
