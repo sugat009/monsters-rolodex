cf2420515d29cf812c3858269bcbeccff7c725d0
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-03-07 10:44:20 +0545

    monsters passed as props to cardlist component, then individual component passed to card component

--- src/App.js
+++ src/App.js
@@ -29,11 +29,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="sugat">
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 
-import './card-list.styles.css';
+import { Card } from "../card/card.component";
+
+import "./card-list.styles.css";
 
 export const CardList = props => {
-  console.log(props);
-
-  return <div className="card-list">{ props.children }</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
